3b38b434c6e5cd50a7baf02dd19742060c128925
Laura Bookman <lbookman@email.arizona.edu>
2019-08-22 11:44:05 -0700

    added messages

--- ArcPro/Scripts/PolyChecker.py
+++ ArcPro/Scripts/PolyChecker.py
@@ -30,7 +30,6 @@ invalid_polygon_found = False
 
 with arcpy.da.SearchCursor(MapUnitPolys, ['SHAPE@', 'MapUnit', 'OBJECTID']) as cursor:
     for row in cursor:
-        #arcpy.AddMessage(str(row[1]))
         # Does this Polygon have a map unit
         if row[1] == "" or row[1] == "<Null>" or row[1] is None or row[1] is 0:
             invalid_polygon_found = True
@@ -39,6 +38,7 @@ with arcpy.da.SearchCursor(MapUnitPolys, ['SHAPE@', 'MapUnit', 'OBJECTID']) as c
 
 # Invalid polygons were found, terminate
 if (invalid_polygon_found):
+    arcpy.AddMessage("Check results and fill in the missing map unit")
     sys.exit(1)
 
 Polygon_Neighbors = "{}/polytest".format(gdb)
@@ -47,12 +47,9 @@ PolygonNeighbor_TableSelect = "{}/PolygonNeighbor_TableSelect".format(gdb)
 
 inFeatures_lyr = "{}/inFeatures_1yr".format(gdb)
 
-dropFields = ["MapUnitPolys_IdentityConfidence", "PolygonNeighbor_TableSelect_NODE_COUNT", "MapUnitPolys_Label", "MapUnitPolys_Notes", "MapUnitPolys_Symbol", "PolygonNeighbor_TableSelect_OBJECTID"]
-
 # Process: Polygon Neighbors
 arcpy.PolygonNeighbors_analysis(MapUnitPolys, Polygon_Neighbors, "OBJECTID;MapUnit", "NO_AREA_OVERLAP", "BOTH_SIDES",
                                 "", "METERS", "SQUARE_METERS")
-								
 
 # Process: Select Layer By Attribute
 arcpy.SelectLayerByAttribute_management(Polygon_Neighbors, "NEW_SELECTION", "src_MapUnit = nbr_MapUnit")
@@ -67,17 +64,14 @@ arcpy.AddMessage(arcpy.GetMessages())
 if int(arcpy.GetCount_management(PolygonNeighbor_TableSelect)[0]) > 0:
     arcpy.MakeFeatureLayer_management(MapUnitPolys, inFeatures_lyr)
 else:
-    print ("done")
+    arcpy.AddMessage("done! No polygons need to be checked")
+    sys.exit(1)
 
 # Process: Add Join
 arcpy.AddJoin_management(inFeatures_lyr, "OBJECTID", PolygonNeighbor_TableSelect, "src_OBJECTID", "KEEP_COMMON")
 
 # Process: Copy Features
 arcpy.CopyFeatures_management(inFeatures_lyr, MapUnitPolys_CopyFeatures, "", "0", "0", "0")
-
-#  Delete Fields in output
-
-arcpy.DeleteField_management(MapUnitPolys_CopyFeatures, dropFields)
 
 # Process: Remove Join
 arcpy.RemoveJoin_management(inFeatures_lyr, "")
